70810fba4c53e194b62ccfa84481e5f0049086c6
Kiran Rawal <rawalkiran19@gmail.com>
2023-05-05 15:21:36 +0545

    Second Version: Improved
    This is the second commit after some changes and improvements in the previous code.

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
 
+import { fileURLToPath } from "url";
+
 //configure env
 dotenv.config();
+
+//esmodule dirname fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 //database config
 connectDB();
@@ -22,16 +29,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-// app.use("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// rest api
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: "Welcome to e-commerce app",
+//   });
 // });
 
 //PORT
